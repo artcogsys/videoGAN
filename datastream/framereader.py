9dcb74653afd582066602944adf0bf92ff219817
florianmahner <fmahner@uos.de>
2018-06-07 16:03:20 +0200

    load batches as videos of single frames. adapt entire reader

--- datastream/framereader.py
+++ datastream/framereader.py
@@ -14,14 +14,18 @@ import os
 import cv2
 import numpy as np
 from multiprocessing import Pool
+from tqdm import tqdm
+import chainer
+from chainer import Variable
+import chainer.functions as F
 
 
-class FrameReader(object):
+class FrameReader(chainer.dataset.DatasetMixin):
     """
     Every FrameReader uses an index file (e.g. job-list.txt) or similar that contains direcotry locations to videos.
     The Videos are supposed to be stored as vertically stacked frames for every second of video. The number of frames
     and frame sizes are then fit into a common framework and each second of the video returned as an individual batch of
-    size. A video of 16 seconds would then be of size: {16, 3 (RGB), n_frames, frame_size, frame_size}
+    size. A video of 16 seconds would then be of size: [16, 3 (RGB), frame_size, frame_size, n_frames]
     """
     def __init__(self, root_dir, index_file, n_frames=32, frame_size=64, ext='.jpg'):
         """
@@ -36,61 +40,59 @@ class FrameReader(object):
         self.ext = ext
         self.n_frames = n_frames
         self.frame_size = frame_size
+        self.content, self.data_size = self.__get_paths()
 
-    def load_dataset(self):
-        """ Loads all data from index file in a multithreaded fashion.
-        :return <list> entire dataset """
+    def __len__(self):
+        return self.data_size
+
+    def get_example(self, idx):
+        if idx > self.data_size:
+            raise IndexError('Index out of range. Not able to retrieve example')
+
+        batch = cv2.imread(self.content[idx])
+        # Get preprocessed batch
+        return self.__preprocess_vid(batch)
+
+    # Is it important that the Framereader reads all batches from a path distinct from each other?!
+
+    def __get_paths(self):
+        """ Make a directory list from text file to read all files in directory with given :attribute: extension, no
+            irrespective of the filenam. Rename to get_paths!"""
 
         with open(os.path.join(self.root_dir, self.index_file)) as f:
-            content = f.readlines()
+            paths = f.readlines()
 
-        # Make a directory list from text file to read all files in directory with given <attribute> extension, no
-        # irrespective of the filename
-        content = [os.path.join(x.strip(), ('*' + self.ext)) for x in content]
+        # Get all path from index file and save all file path in its subdirs
+        all_paths =[]
+        for path in paths:
+            if path.strip():  # Ignore empty lines from filereader
+                file_paths = os.path.join(path.strip(), ('*' + self.ext))
+                all_paths.extend(glob.glob(file_paths))
 
-        # Load all images from directories and pass onto thread pool for parallel processing. Images are first loaded
-        # and then reshaped into a common dimensionality framework
-        pool = Pool()
-        images = pool.map(self._load_imgs, content)
-        batch = pool.map(self._preprocess_imgs, [images[i] for i in range(len(images))])
-        batch = np.concatenate(batch)
-        pool.close()
-        pool.join()
+        return all_paths, len(all_paths)
 
-        return batch
-
-    def _load_imgs(self, relative_path):
-        """ Load all images as array into single list """
-        image_list = list(map(cv2.imread, glob.glob(relative_path)))
-        return image_list
-
-    def _preprocess_imgs(self, images):
+    def __preprocess_vid(self, video):
 
         xp = cuda.get_array_module()
 
-        # Preallocate data for computational speed and retrieve attributes once
-        img_proc = np.empty([len(images), 3, self.frame_size, self.frame_size, self.n_frames], dtype=np.float32)
         frame_size = self.frame_size
         n_frames = self.n_frames
 
-        for idx, img in enumerate(images):
+        r = frame_size / video.shape[1] # Scaling factor
+        dim = (frame_size, (int(video.shape[0] * r)))
 
-            r = frame_size / img.shape[1] # Scaling factor
-            dim = (frame_size, (int(img.shape[0] * r)))
-            # Rescale the image to fit the actual frame size. A video of e.g. of original size {128 x 128} is then
-            # reduced to {frame_size x frame_size}
-            img = cv2.resize(img, dim)
-            frames_video = img.shape[0] // frame_size
-            img = xp.reshape(img, [3, 64, 64, frames_video])
+        # Rescale the image to fit the actual frame size. A video of e.g. of original size {128 x 128} is then
+        # reduced to {frame_size x frame_size}
+        video = cv2.resize(video, dim)
+        frames_video = video.shape[0] // frame_size
+        video = xp.reshape(video, [3, 64, 64, frames_video])
 
-            # Either take the first n_frames, or if the frame length is too short, create repeated copies of last frame
-            # and append
-            if frames_video >= n_frames:
-                img = img[:, :, :, 0 : n_frames]
-            else:
-                last = xp.tile(img[-1], (1, 1, 1, n_frames - frames_video))
-                img = xp.concatenate((img, last))
+        # Either take the first n_frames, or if the frame length is too short, create repeated copies of last frame
+        # and append
+        if frames_video >= n_frames:
+            video = video[:, :, :, 0 : n_frames]
+        else:
+            last = xp.tile(video[-1], (1, 1, 1, n_frames - frames_video))
+            video = xp.concatenate((video, last))
 
-            img_proc[idx] = img
-
-        return img_proc
+        return video
