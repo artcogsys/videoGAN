c30977cc9ebd944d687c46dc8954b1f8bab8f3d3
florianmahner <fmahner@uos.de>
2018-06-22 12:40:59 +0200

    Remove layer norm and own weights and correct error parsing weights for new test run

--- nn/DCGAN.py
+++ nn/DCGAN.py
@@ -14,17 +14,30 @@ For an introduction, it is referred to:
 import chainer
 import chainer.functions as F
 import chainer.links as L
-from chainer import cuda
+import numpy as np
+
+
+def convolution3d(in_channel, out_channel, weights, k_size=(4,4,4), stride=(2,2,2), pad=1, direction='forward'):
+    """ Convolutions - A convolutional block consists of: (n_dim, in_channel, out_channel, ksize, stride, pad).
+    n_dim is set to 3 for 3D videos. Depending on the direction, each convolution propagates from the dimension of
+    the input channel to the output channel"""
+    if direction == 'forward':
+        return L.ConvolutionND(3, in_channel, out_channel, k_size, stride, pad=pad, initialW=weights)
+    elif direction == 'backward':
+        return L.DeconvolutionND(3, in_channel, out_channel, k_size, stride, pad=pad, initialW=weights)
+    else:
+        raise NameError('Wrong direction specified, please choose between {forward, backward}')
+
 
 class VideoGAN(chainer.Chain):
     """ Superclass that holds shared attributes and parameters for the Discriminator and Generator """
     def __init__(self, **kwargs):
         super(VideoGAN, self).__init__()
         self._batch_size = kwargs.get('batch_size', 64)
-        self._n_frames = kwargs.get('n_frames', 32)
-        self._frame_size = kwargs.get('frame_size', 64)
         self._latent_dim = kwargs.get('latent_dim', 100)
+        self._n_frames = kwargs.get('n_frames', 32)
         self._weight_scale = kwargs.get('weight_scale', .001)
+        self._frame_size = kwargs.get('frame_size', 64)
 
     @property
     def batch_size(self):
@@ -42,37 +55,6 @@ class VideoGAN(chainer.Chain):
         """ Getter method for number of frames"""
         return self._n_frames
 
-    def convolution3d(self, in_channel, out_channel, weights, k_size=(4, 4, 4), stride=(2, 2, 2), pad=1, direction='forward'):
-        """ Convolutions - A convolutional block consists of: (n_dim, in_channel, out_channel, ksize, stride, pad).
-        n_dim is set to 3 for 3D videos. Depending on the direction, each convolution propagates from the dimension of
-        the input channel to the output channel"""
-
-        xp = self.xp
-
-        def __init_weights(in_dim, out_dim):
-
-            def uniform(std_dev, size):
-                return xp.random.uniform(low=-std_dev * xp.sqrt(3), high=std_dev * xp.sqrt(3), size=size).astype(
-                    'float32')
-
-            fan_in = in_dim * xp.prod(k_size)
-            fan_out = (out_channel * xp.prod(k_size)) / (xp.prod(stride))
-
-            filters_std = xp.sqrt(4. / (fan_in + fan_out))
-
-            filter_values = uniform(filters_std, (out_dim, in_dim, k_size[0], k_size[1], k_size[2]))
-
-            return filter_values
-
-        if direction == 'forward':
-            weights = __init_weights(in_channel, out_channel)
-            return L.ConvolutionND(3, in_channel, out_channel, k_size, stride, pad=pad, initialW=weights)
-        elif direction == 'backward':
-            weights = __init_weights(out_channel, in_channel)
-            return L.DeconvolutionND(3, in_channel, out_channel, k_size, stride, pad=pad, initialW=weights)
-        else:
-            raise NameError('Wrong direction specified, please choose between {forward, backward}')
-
 
 class Discriminator(VideoGAN):
     """ The discriminator network consists of five convolutional layers. The first convolutional layer has three input
@@ -84,7 +66,7 @@ class Discriminator(VideoGAN):
     is also omitted. See http://carlvondrick.com/tinyvideo/discriminator.png for a visualization of tensor sizes with
     increasing layer order."""
 
-    def __init__(self, ch=64, **kwargs):
+    def __init__(self, ch = 64, **kwargs):
         # Initial channel is the number of feature channels in the first convolutional layer.
         self.ch = ch
         super(Discriminator, self).__init__(**kwargs)
@@ -96,52 +78,26 @@ class Discriminator(VideoGAN):
 
             # Convolutions. A kernel size of 4, stride 2 and pad 1 is used for forward propagation. The padding is
             # required to fit dimensionality. Propagate from 64 to 128 to 256 to 512 channels.
-            self.conv1 = self.convolution3d(3, ch, self.w)
-            self.conv2 = self.convolution3d(ch, ch * 2, self.w)
-            self.conv3 = self.convolution3d(ch * 2, ch * 4, self.w)
-            self.conv4 = self.convolution3d(ch * 4, ch * 8, self.w)
+            self.conv1 = convolution3d(3, ch, self.w)
+            self.conv2 = convolution3d(ch, ch * 2, self.w)
+            self.conv3 = convolution3d(ch * 2, ch * 4, self.w)
+            self.conv4 = convolution3d(ch * 4, ch * 8, self.w)
 
             # Convolute from 4x4x2 to 1x1x1 tensor in the last layer by changing the ksize and removing the padding
-            self.conv5 = self.convolution3d(ch * 8, 1, self.w, k_size=(4,4,2), pad=0)
+            self.conv5 = convolution3d(ch * 8, 1, self.w, k_size=(4,4,2), pad=0)
             self.linear5 = L.Linear(1, 1, initialW=self.w)
 
-            self.layer_norm1 = L.LayerNormalization()
-            self.layer_norm2 = L.LayerNormalization()
-            self.layer_norm3 = L.LayerNormalization()
-            self.layer_norm4 = L.LayerNormalization()
-
     def __call__(self, input):
-        """ Procedure: Convolute forward from each hidden unit amd then activate (no pooling!). All except the linear
-            downsampling layer have a layer normalization, that accepts inputs of shape (batch_size, unit_size) and is
-            afterwards reshaped to pro
-        
+        """ Procedure: Convolute forward from each hidden unit amd then activate (no pooling!).
+
         input shape: (batch size, 3(RGB), 64, 64, 32)
         return shape: (batch size, 1)
         """
-
-        hidden1 = self.conv1(input)
-        h1_shape = hidden1.shape
-        hidden1 = self.layer_norm1(F.reshape(hidden1, [self.batch_size, -1]))
-        hidden1 = F.leaky_relu(hidden1)
-
-        hidden2 = self.conv2(F.reshape(hidden1, h1_shape))
-        h2_shape = hidden2.shape
-        hidden2 = self.layer_norm2(F.reshape(hidden2, [self.batch_size, -1]))
-        hidden2 = F.leaky_relu(hidden2)
-
-        hidden3 = self.conv3(F.reshape(hidden2, h2_shape))
-        h3_shape = hidden3.shape
-        hidden3 = self.layer_norm3(F.reshape(hidden3, [self.batch_size, -1]))
-        hidden3 = F.leaky_relu(hidden3)
-
-        hidden4 = self.conv4(F.reshape(hidden3, h3_shape))
-        h4_shape = hidden4.shape
-        hidden4 = self.layer_norm4(F.reshape(hidden4, [self.batch_size, -1]))
-        hidden4 = F.leaky_relu(hidden4)
-
-        hidden5 = self.conv5(F.reshape(hidden4, h4_shape))
-        hidden5 = F.leaky_relu(hidden5)
-
+        hidden1 = F.leaky_relu(self.conv1(input))
+        hidden2 = F.leaky_relu(self.conv2(hidden1))
+        hidden3 = F.leaky_relu(self.conv3(hidden2))
+        hidden4 = F.leaky_relu(self.conv4(hidden3))
+        hidden5 = F.leaky_relu(self.conv5(hidden4))
         return self.linear5(hidden5)
 
 
@@ -151,7 +107,7 @@ class Generator(VideoGAN):
     For a visualization, please see: http://carlvondrick.com/tinyvideo/network.png.
     """
 
-    def __init__(self, ch=512, **kwargs):
+    def __init__(self, ch = 512, **kwargs):
         self.ch = ch
         super(Generator, self).__init__(**kwargs)
         self._up_sample_dim = tuple([ch, 4, 4, 2])
@@ -166,10 +122,10 @@ class Generator(VideoGAN):
 
             # Convolutions. Perform a series of four fractionally-strided convolutions which map to 256,128,64,3
             # feature channels, producing of video of size (64,64,32,3)
-            self.deconv1 = self.convolution3d(ch, ch // 2, weights = self.w, direction = 'backward', pad=1)
-            self.deconv2 = self.convolution3d(ch // 2, ch // 4, weights = self.w, direction = 'backward', pad=1)
-            self.deconv3 = self.convolution3d(ch // 4, ch // 8, weights = self.w, direction = 'backward', pad=1)
-            self.deconv4 = self.convolution3d(ch // 8, 3, weights = self.w, direction = 'backward', pad=1)
+            self.deconv1 = convolution3d(ch, ch // 2, self.w, direction = 'backward', pad=1)
+            self.deconv2 = convolution3d(ch // 2, ch // 4, self.w, direction = 'backward', pad=1)
+            self.deconv3 = convolution3d(ch // 4, ch // 8, self.w, direction = 'backward', pad=1)
+            self.deconv4 = convolution3d(ch // 8, 3, self.w, direction = 'backward', pad=1)
 
             # Batch normalizations for all layers except last one
             self.batch_norm1 = L.BatchNormalization(self._out_size)
